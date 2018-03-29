28206aaf6cd50d39c7fd14d413a9cb08c3d713d3
root <root@gmail.com>
2018-03-29 09:21:34 +0000

    add .

---
+++ myapps/src/main/java/infosys/App.java
@@ -0,0 +1,13 @@
+package infosys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
